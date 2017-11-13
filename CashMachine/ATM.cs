c92f0bafbd643d367ae3b9a61d08fa5ac398705e
poddubnyy@ukr.net <poddubnyy@ukr.net>
2017-11-13 21:11:59 +0300

    home work made 13.11.17

---
+++ CashMachine/ATM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class ATM
+    {
+        private int Namber { get; }
+        private string Adress { get; }
+        private int CurrentRest { get; set; }
+        private int MinRest { get; }
+        private Action<int, string, int> FewManey;
+        private Action<int> NoManey;
+        public  ATM(int _namber, string _adress,Action<int, string, int> _func_few, Action<int> _func_no)
+        {
+            Namber = _namber;
+            Adress = _adress;
+            CurrentRest = 20000;
+            MinRest = 2000;
+            FewManey = _func_few;
+            NoManey = _func_no;
+        }
+        public void WithdrawMoney(int summ)
+        {
+             if(CurrentRest-summ<0)
+            {
+                NoManey(summ);
+                return;
+             }
+            CurrentRest -= summ;
+            Console.WriteLine($"Take your {summ} $ please.");
+            if (CurrentRest < MinRest)
+            {
+                FewManey(Namber, Adress, CurrentRest);
+                AddMoney(10000);
+            }
+        }
+        public void AddMoney(int summ)
+        {
+            CurrentRest += summ;
+        }
+        public override string ToString()
+        {
+            return $"Rest is {CurrentRest} $";
+        }
+    }
+}
---
+++ CashMachine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, string, int> Messenge = new Action<int, string, int>(encashment);
+            Messenge += menegers;
+            Action<int> Displey = new Action<int>(disleymessege);
+            ATM CashMachine = new ATM(125, "Televizionaya street 4-a", Messenge, Displey);
+            do
+            {
+                Console.WriteLine("\n"+CashMachine.ToString());
+                Console.WriteLine("Set Summ please!");
+                int Summ = Convert.ToInt32(Console.ReadLine());
+                CashMachine.WithdrawMoney(Summ);
+                Console.WriteLine("Set esc for exite please!");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        }
+        static void encashment(int _nomber,string _adress, int _currentRest)
+        {
+            Console.WriteLine($"Encashment! In ATM nomber {_nomber}, adress {_adress} rest {_currentRest} $");
+        }
+        static void menegers(int _nomber, string _adress, int _currentRest)
+        {
+            Console.WriteLine($"Menegers! In ATM nomber {_nomber}, adress {_adress} rest {_currentRest} $");
+        }
+        static void disleymessege(int _summ)
+        {
+            Console.WriteLine($"ATM don't have {_summ} $! Set other summ please!");
+        }
+    }
+}
